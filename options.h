a43c7c4c647586cc1f79f621ad1b40fae9fe9e7e
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-08-23 20:30:01 +0800

    adding options dialog

---
+++ options.h
@@ -0,0 +1,17 @@
+#ifndef OPTIONS_H
+#define OPTIONS_H
+
+#include <QWidget>
+
+class options : public QWidget
+{
+  Q_OBJECT
+public:
+  explicit options(QWidget *parent = nullptr);
+  
+signals:
+  
+public slots:
+};
+
+#endif // OPTIONS_H
