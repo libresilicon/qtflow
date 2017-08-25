8949c3c55239738a97d03a74987dd1b2e61aadfc
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-08-26 01:53:57 +0800

    adding new editor class

---
+++ schematicseditor.cpp
@@ -0,0 +1,6 @@
+#include "schematicseditor.h"
+
+SchematicsEditor::SchematicsEditor()
+{
+
+}
---
+++ schematicseditor.h
@@ -0,0 +1,12 @@
+#ifndef SCHEMATICSEDITOR_H
+#define SCHEMATICSEDITOR_H
+
+#include <QWidget>
+
+class SchematicsEditor
+{
+public:
+	SchematicsEditor();
+};
+
+#endif // SCHEMATICSEDITOR_H
