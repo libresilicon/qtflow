7b1e517052da3e38c8dc23a2172f4a7347a9a780
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-08-25 14:54:30 +0800

    adding toolbar class

---
+++ editortoolbar.cpp
@@ -0,0 +1,6 @@
+#include "editortoolbar.h"
+
+EditorToolBar::EditorToolBar(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ editortoolbar.h
@@ -0,0 +1,17 @@
+#ifndef EDITORTOOLBAR_H
+#define EDITORTOOLBAR_H
+
+#include <QWidget>
+
+class EditorToolBar : public QWidget
+{
+	Q_OBJECT
+public:
+	explicit EditorToolBar(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // EDITORTOOLBAR_H
