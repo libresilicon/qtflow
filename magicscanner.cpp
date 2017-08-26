d90ee5bc7c9f297b31a385debde2a4ae1e2b8c15
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-08-26 15:12:25 +0800

    adding wrapper class

---
+++ magicscanner.cpp
@@ -0,0 +1,6 @@
+#include "magicscanner.h"
+
+MagicScanner::MagicScanner()
+{
+
+}
---
+++ magicscanner.h
@@ -0,0 +1,11 @@
+#ifndef MAGICSCANNER_H
+#define MAGICSCANNER_H
+
+
+class MagicScanner
+{
+public:
+	MagicScanner();
+};
+
+#endif // MAGICSCANNER_H
