8c7dc2b6c57d77d7b6b5f6022dde9594981510ee
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-08-26 17:46:00 +0800

    updating wrapper class

--- magicdata.cpp
+++ magicdata.cpp
@@ -1,6 +1,30 @@
 #include "magicdata.h"
 
-MagicData::MagicData()
-{
+namespace magic {
+	MagicData::MagicData()
+	{
 
+	}
+
+	void MagicData::clearParsedElements()
+	{
+		parsedElements.clear();
+	}
+
+	void MagicData::setTitle(QString s)
+	{
+		title = s;
+	}
+
+	class MagicScanner *MagicData::getLexer()
+	{
+		return lexer;
+	}
+
+	void MagicData::addRectangle(int x, int y, int w, int h, QString m)
+	{
+		// TODO: Add string for identification!!
+		QRect objR(x,y,w,h);
+		parsedElements.append(objR);
+	}
 }
--- magicdata.h
+++ magicdata.h
@@ -1,11 +1,28 @@
 #ifndef MAGICDATA_H
 #define MAGICDATA_H
 
+#include <QString>
+#include <QVector>
+#include <QRect>
 
-class MagicData
-{
-public:
-	MagicData();
-};
+#include "magicscanner.h"
+
+typedef QVector<QRect> rects_t;
+
+namespace magic {
+	class MagicData
+	{
+	public:
+		MagicData();
+		void clearParsedElements();
+		void addRectangle(int x, int y, int w, int h, QString m);
+		void setTitle(QString s);
+		class MagicScanner *getLexer();
+	private:
+		rects_t parsedElements;
+		class MagicScanner* lexer;
+		QString title;
+	};
+}
 
 #endif // MAGICDATA_H
