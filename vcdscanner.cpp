a1ab2a6cc5ed8caca41f24739219ce55b007ef7d
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-08-26 15:07:34 +0800

    adding wrapper class

---
+++ vcdscanner.cpp
@@ -0,0 +1,6 @@
+#include "vcdscanner.h"
+
+VCDScanner::VCDScanner()
+{
+
+}
---
+++ vcdscanner.h
@@ -0,0 +1,11 @@
+#ifndef VCDSCANNER_H
+#define VCDSCANNER_H
+
+
+class VCDScanner
+{
+public:
+	VCDScanner();
+};
+
+#endif // VCDSCANNER_H
