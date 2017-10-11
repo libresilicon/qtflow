30622ac0b6c92a6ea99007ff8312605a2bfda1aa
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-10-11 19:51:43 +0800

    generating KiCAD readible library files from LEF files

--- tech/osu035/generate_schematic_symbols.py
+++ tech/osu035/generate_schematic_symbols.py
@@ -9,15 +9,26 @@ macro_mode = False
 first_pin = True
 
 file = open("osu035_stdcells.lef","r")
-outfile = open("osu035_stdcells.sym","w")
+outfile = open("osu035_stdcells.slib","w")
+
+outfile.write("EESchema-LIBRARY Version 2.3\n")
+outfile.write("#encoding utf-8\n")
+
 for line in file:
 	if re.search("MACRO" , line):
 		macro_mode = True
 		first_pin = True
+		idx = 0
 		has_pins = False
+		output_pos = -100
+		input_pos = -100
 		l = line.split()
 		macro_name = l[1]
-		outl = "DEF "+macro_name+" U 0 30 Y Y F N\n"
+		outl = "DEF "+macro_name+" U 0 30 Y Y 1 F N\n"
+		outl += "F0 \"U\" 0 50 60 H V C CNN\n"
+		outl += "F1 \""+macro_name+"\" 0 -50 60 H V C CNN\n"
+		outl += "F2 \"\" 0 0 60 H I C CNN\n"
+		outl += "F3 \"\" 0 0 60 H I C CNN\n"
 		outfile.write(outl)
 
 	if re.search("PIN" , line):
@@ -46,29 +57,36 @@ for line in file:
 				first_pin = False
 
 			if(pin_use=="POWER"):
-				outl = "X "+pin_name+" -150 150 0 D 50 30 0 0 W P\n"
+				outl = "X "+pin_name+" "+str(idx)+" 0 150 100 U 50 30 1 1 W\n"
 				outfile.write(outl)
 			elif(pin_use=="GROUND"):
-				outl = "X "+pin_name+" -150 150 0 D 50 30 0 0 W P\n"
+				outl = "X "+pin_name+" "+str(idx)+" 0 -150 100 D 50 30 1 1 P\n"
 				outfile.write(outl)
 			elif(pin_direction=="INPUT"):
-				outl = "X "+pin_name+" -150 150 0 D 50 30 0 0 W I\n"
+				outl = "X "+pin_name+" "+str(idx)+" -250 "+str(input_pos)+" 100 L 50 30 1 1 I\n"
+				input_pos+=80
 				outfile.write(outl)
 			elif(pin_direction=="OUTPUT"):
-				outl = "X "+pin_name+" -150 150 0 D 50 30 0 0 W O\n"
+				outl = "X "+pin_name+" "+str(idx)+" 250 "+str(output_pos)+" 100 R 50 30 1 1 O\n"
+				output_pos+=80
 				outfile.write(outl)
 			elif(pin_direction=="INOUT"):
-				outl = "X "+pin_name+" -150 150 0 D 50 30 0 0 W P\n"
+				outl = "X "+pin_name+" "+str(idx)+" 250 "+str(output_pos)+" 100 R 50 30 1 1 I\n"
+				output_pos+=80
 				outfile.write(outl)
 
 			pin_mode = False
+			idx+=1
 
 	if re.search("END" , line) and re.search(macro_name , line):
 		if(macro_mode):
+			outl = ""
 			if(has_pins):
-				outl = "ENDDRAW\n"
-				outfile.write(outl)
-			outl = "ENDDEF\n"
+				outl += "C 450 250 0 0 1 0 N\n"
+				outl += "S 250 -150 -250 150 0 1 0 N\n"
+				outl += "ENDDRAW\n"
+
+			outl += "ENDDEF\n"
 			outfile.write(outl)
 			macro_mode = False
 			
