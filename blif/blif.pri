5e276dcf4fe19518005e8dd71af855e695f0fa88
David Lanzendörfer <david.lanzendoerfer@o2s.ch>
2017-10-30 17:27:03 +0800

    removing files

--- blif/blif.pri
+++ blif/blif.pri
@@ -3,9 +3,6 @@ PARSE_OUT_PWD = $$OUT_PWD/blif
 # This was needed to work around "number of sections exceeded object file format limit" linker error
 win32:QMAKE_CXXFLAGS += /bigobj
 
-FLEXSOURCES += $$PWD/blif_scanner.ll
-BISONSOURCES += $$PWD/blif_parser.yy
-
 SOURCES += \
 	$$PWD/blifscanner.cpp \
 	$$PWD/blifdata.cpp
--- blif/blifdata.cpp
+++ blif/blifdata.cpp
@@ -4,18 +4,11 @@
 #include <QDebug>
 
 namespace blif {
-	BLIFModuleInfo::BLIFModuleInfo() {}
-
 	BLIFData::BLIFData(QString filename) :
 		lexer(NULL),
 		parser(NULL),
 		trace_scanning(false),
-		trace_parsing(false),
-		distanceMicrons(false),
-		m_BBLowerX(0),
-		m_BBLowerY(0),
-		m_BBUpperX(0),
-		m_BBUpperY(0)
+		trace_parsing(false)
 	{
 		std::ifstream input;
 		std::string stdfilename = filename.toStdString();
@@ -32,65 +25,14 @@ namespace blif {
 		input.close();
 	}
 
-	void BLIFData::setAmountComponents(int i)
-	{
-		amountComponents = i;
-	}
-
-
-	void BLIFData::addUsedModuleNames(std::string *instance_name, std::string *macro_name)
-	{
-		recent_module.macro_name = QString::fromStdString(*macro_name);
-		recent_module.instance_name = QString::fromStdString(*instance_name);
-	}
-
-	void BLIFData::addUsedModulePlacement(double x, double y)
-	{
-		recent_module.x = x;
-		recent_module.y = y;
-	}
-
-	BLIFScanner *BLIFData::getLexer()
+	BLIFScanner* BLIFData::getLexer()
 	{
 		return lexer;
 	}
 
-	void BLIFData::setDistanceUnitMicrons(int t)
+	void BLIFData::setModelName(std::string s)
 	{
-		distanceMicrons = true;
-		distanceMicronsValue = t;
+		m_modelName = QString::fromStdString(s);
 	}
 
-	int BLIFData::getLowerX()
-	{
-		return m_BBLowerX;
-	}
-
-	int BLIFData::getLowerY()
-	{
-		return m_BBLowerY;
-	}
-
-	int BLIFData::getUpperX()
-	{
-		return m_BBUpperX;
-	}
-
-	int BLIFData::getUpperY()
-	{
-		return m_BBUpperY;
-	}
-
-	void BLIFData::setDieArea(int x1, int x2, int y1, int y2)
-	{
-		m_BBLowerX = x1;
-		m_BBUpperX = x2;
-		m_BBLowerY = y1;
-		m_BBUpperY = y2;
-	}
-
-	int BLIFData::getDistanceUnit()
-	{
-		return distanceMicronsValue;
-	}
 }
